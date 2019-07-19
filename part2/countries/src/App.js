cf30e333b744fb9c11bb110de305f41c0479a564
Alfonso Gutierrez <agutiernc@gmail.com>
2019-07-18 22:14:01 -0700

    2.12 Data for countries - step1

---
+++ part2/countries/src/App.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Display = ({country}) => {
+        return (
+            <p>{country.name}</p>
+        )
+}
+
+const Dialect = ({language}) => {
+    return (
+        <li>{language.name}</li>
+    )
+}
+
+const Country = ({country}) => {
+    const countryDialect = () => country.languages.map(item => 
+       <Dialect
+            key={item.iso639_1}
+            language = {item}
+       />
+    )
+
+    return (
+        <div>
+            <h1>{country.name}</h1>
+            <p>Capital: {country.capital}</p>
+            <p>Population: {country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+            <div>
+                <h2>Languages</h2>
+                <ul>
+                   {countryDialect()}
+                </ul>
+            </div>
+            <div>
+                <img src={country.flag} width="200" alt="Country's flag" />
+            </div>
+        </div>
+    )
+}
+
+const DisplayCountries = ({countries, filterCountry}) => {
+    if(filterCountry !== ''){
+
+        const filterCountries = countries.filter(country => country.name.toLowerCase().includes(filterCountry.toLowerCase()))
+
+        if(filterCountries.length > 10) {
+            return (
+                <p>Too many matches. Specify another filter</p>
+            )
+        }
+
+        if(filterCountries.length === 1) {
+            return (
+                <Country country={filterCountries[0]} />
+            )
+        }
+
+        const rows = () => filterCountries.map(country =>
+            <Display
+                key={country.numericCode}
+                country={country}
+            />
+        )
+    
+        return (
+            <>
+                {rows()}
+            </>
+        )
+        
+    } else {
+        return (
+            <></>
+        )
+    }
+}
+
+const App = () => {
+    const [countries, setCountries] = useState([])
+    const [filterCountry, setFilterCountry] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(response => setCountries(response.data))
+    }, [])
+
+    const handleFilterChange = (event) => {
+        setFilterCountry(event.target.value)
+    }
+
+    return (
+        <div>
+            <div>
+                Find Countries: <input value={filterCountry} onChange={handleFilterChange} />
+            </div>
+
+            <div>
+                <DisplayCountries countries={countries} filterCountry={filterCountry} />
+            </div>
+
+        </div>
+    )
+}
+
+export default App
---
+++ part2/countries/src/components/Display.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Display = ({country}) => {
+
+    return (
+        <p>{country.name}</p>
+    )
+}
+
+export default Display

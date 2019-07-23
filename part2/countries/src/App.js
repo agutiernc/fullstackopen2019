3c9c16e4892f922de92b1c6c27b52ed8c22754c7
Alfonso Gutierrez <agutiernc@gmail.com>
2019-07-22 19:12:18 -0700

    2.14: Data for countries-step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const Weather = ({ city }) => {
+    const [showWeather, setShowWeather] = useState('')
+
+    useEffect(() => {
+        axios
+            .get(`http://api.apixu.com/v1/current.json?key=25e2494250424b12963234812192207&q=${city}`)
+            .then(response => setShowWeather(response.data))
+    }, [city])
+
+    if (!showWeather) {
+        return (
+            <></>
+        )
+    }
+
+    return (
+        <div>
+            <h2>Weather in {city}</h2>
+            <p><strong>Temperature:</strong> {showWeather.current.temp_f}&deg;F</p>
+            <img src={showWeather.current.condition.icon} alt={showWeather.current.condition.text} />
+            <p><strong>Wind: </strong>
+                {showWeather.current.wind_mph} mph <strong>Direction:</strong> {showWeather.current.wind_dir}
+            </p>
+        </div>
+    )
+}
+
 const Display = ({ country }) => {
     const [showCountry, setShowCountry] = useState(true)
 
@@ -50,6 +77,7 @@ const Country = ({country}) => {
             <div>
                 <img src={country.flag} width="200" alt="Country's flag" />
             </div>
+            <Weather city={country.capital} />
             <br />
         </div>
     )
